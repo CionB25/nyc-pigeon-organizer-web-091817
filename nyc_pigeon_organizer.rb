0867cae15c5b6a65a40c02d7ffb2bc6ff159326c
Kyle Andrews <kylemandrews@gmail.com>
2017-09-13 23:18:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+
+  data.each do |descrip, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+
+        if !pigeon_list[name].has_key?(descrip)
+          pigeon_list[name][descrip] = []
+        end
+
+        if !pigeon_list[name][descrip].include?(attribute)
+          pigeon_list[name][descrip] << attribute.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
